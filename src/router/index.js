198f23efdead4487aa6723a20d45fb3c2b89ceef
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-07-30 13:12:31 +1200

    Allow for different versions of the Sphinx XML to be handled.

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,19 @@ const routes = [
       import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
-    path: '/documentation/:pageName*',
+    path: '/documentation/:version/:pageName*',
     name: 'Documentation',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "documentation" */ '../views/Documentation.vue'
+      ),
+  },
+  {
+    path: '/documentation',
+    name: 'DocumentationRoot',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,16 +1,84 @@
 <template>
   <div class="documentation">
-    <sphinx-page baseURL="/sphinx-xml-files" />
+    <template v-if="isRoot">
+      <ul>
+        <li>
+          <router-link
+            :to="{
+              name: 'Documentation',
+              params: { version: 'v0.1.2' },
+            }"
+            >v0.1.2</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            :to="{
+              name: 'Documentation',
+              params: { version: 'v0.3.1' },
+            }"
+            >v0.3.1</router-link
+          >
+        </li>
+      </ul>
+    </template>
+    <template v-else>
+      <v-select
+        :options="['v0.1.2', 'v0.3.1']"
+        :clearable="false"
+        :closeOnSelect="true"
+        :value="version"
+        @input="setSelected"
+      ></v-select>
+      <sphinx-page :baseURL="`/sphinx-xml-files/${this.version}`" />
+    </template>
   </div>
 </template>
 
 <script>
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
 import { SphinxPage } from 'vue-sphinx-xml'
 
 export default {
   name: 'Documentation',
   components: {
     SphinxPage,
+    vSelect,
+  },
+  data() {
+    return {
+      version: '',
+    }
+  },
+  watch: {
+    $route(to) {
+      if (to.params.version !== this.version) {
+        this.version = to.params.version
+      }
+    },
+  },
+  created() {
+    this.version = this.$route.params.version
+  },
+  computed: {
+    isRoot() {
+      return this.$route.name === 'DocumentationRoot'
+    },
+  },
+  methods: {
+    setSelected(value) {
+      if (value) {
+        this.version = value
+        if (this.$route.params.version !== value) {
+          this.$router.push({
+            name: 'Documentation',
+            params: { version: value, pageName: this.$route.params.pageName },
+          })
+        }
+      }
+    },
   },
 }
 </script>
